ce7e522c50c1d53b2ce7e8a5c978a2d4dd6ca58f
Vyacheslav Rusakov <vyarus@gmail.com>
2019-02-07 15:03:37 +0700

    add empty views configuration tests

---
+++ guicey-server-pages/src/test/groovy/ru/vyarus/guicey/gsp/views/PrintEmptyConfigurationTest.groovy
@@ -0,0 +1,42 @@
+package ru.vyarus.guicey.gsp.views
+
+import io.dropwizard.Application
+import io.dropwizard.Configuration
+import io.dropwizard.setup.Bootstrap
+import io.dropwizard.setup.Environment
+import ru.vyarus.dropwizard.guice.test.spock.ConfigOverride
+import ru.vyarus.dropwizard.guice.test.spock.UseDropwizardApp
+import ru.vyarus.guicey.gsp.ServerPagesBundle
+import spock.lang.Specification
+
+/**
+ * @author Vyacheslav Rusakov
+ * @since 07.02.2019
+ */
+@UseDropwizardApp(value = App, configOverride = [
+        @ConfigOverride(key = "server.rootPath", value = "/rest/*")
+])
+class PrintEmptyConfigurationTest extends Specification {
+
+    def "Check empty config printing"() {
+
+        expect: "created main config empty map correctlu prointed"
+        true
+    }
+
+    static class App extends Application<Configuration> {
+
+        @Override
+        void initialize(Bootstrap<Configuration> bootstrap) {
+            // pure dropwizard bundle
+            bootstrap.addBundle(ServerPagesBundle.app("app", "/app", "/")
+                    .viewsConfiguration({ null })
+                    .printViewsConfiguration()
+                    .build())
+        }
+
+        @Override
+        void run(Configuration configuration, Environment environment) throws Exception {
+        }
+    }
+}
---
+++ guicey-server-pages/src/test/groovy/ru/vyarus/guicey/gsp/views/ViewsConfigCreationTest.groovy
@@ -0,0 +1,44 @@
+package ru.vyarus.guicey.gsp.views
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import io.dropwizard.Application
+import io.dropwizard.Configuration
+import io.dropwizard.setup.Bootstrap
+import io.dropwizard.setup.Environment
+import ru.vyarus.dropwizard.guice.test.spock.ConfigOverride
+import ru.vyarus.dropwizard.guice.test.spock.UseDropwizardApp
+import ru.vyarus.guicey.gsp.ServerPagesBundle
+import spock.lang.Specification
+
+/**
+ * @author Vyacheslav Rusakov
+ * @since 07.02.2019
+ */
+@UseDropwizardApp(value = App, configOverride = [
+        @ConfigOverride(key = "server.rootPath", value = "/rest/*")
+])
+class ViewsConfigCreationTest extends Specification {
+
+    def "Check null views configuration binding"() {
+
+        expect: "created main config map and sub map for freemarker"
+        true
+    }
+
+    static class App extends Application<Configuration> {
+
+        @Override
+        void initialize(Bootstrap<Configuration> bootstrap) {
+            // pure dropwizard bundle
+            bootstrap.addBundle(ServerPagesBundle.app("app", "/app", "/")
+                    .viewsConfiguration({ null })
+                    .viewsConfigurationModifier('freemarker', { assert it != null })
+                    .printViewsConfiguration()
+                    .build())
+        }
+
+        @Override
+        void run(Configuration configuration, Environment environment) throws Exception {
+        }
+    }
+}
